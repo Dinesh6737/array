6045743ca30d10ec4f119d33834785d6caa8bdf0
dinesh6737 <dinesh.dk220@gmail.com>
2020-01-21 13:38:08 +0530

    sample

---
+++ src/org/ary/ArrayProgram.java
@@ -0,0 +1,25 @@
+package org.ary;
+
+import java.util.ArrayList;
+
+public class ArrayProgram {
+	public static void main(String[] args) {
+		ArrayList<Integer> list = new ArrayList<Integer>();  
+		list.add(10);
+		list.add(20);
+		list.add(30);
+		list.add(90);
+		list.add(10);
+		list.add(10);
+		list.add(40);
+		list.add(50);
+		
+		System.out.println(list.size());
+		
+
+		
+
+
+	}
+
+}
